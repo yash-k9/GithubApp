48693ccba53b638b5bc5fbbb1f16821e57cf85c4
Kurella Yash <yashkurella.9@gmail.com>
2022-01-22 23:01:26 +0530

    Add Package Structure

--- app/src/main/java/com/yashk9/githubapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/yashk9/githubapp/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yashk9.githubapp
+package com.yashk9.githubapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yashk9.githubapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
